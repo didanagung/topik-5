d2fe39752382b515d3bdb50bb85814f8cb4d2b9b
didanagung <familytidai@gmail.com>
2020-06-03 12:07:38 +0700

    topik-5

---
+++ ci-app/application/models/Mahasiswa_model.php
@@ -0,0 +1,27 @@
+<?php
+
+class Mahasiswa_model extends CI_Model
+{
+    public function getAllMahasiswa()
+    {
+        return $this->db->get('mahasiswa')->result_array();
+    }
+
+    public function tambahDataMahasiswa()
+    {
+        $data = [
+            "nama" => $this->input->post('nama', true),
+            "nrp" => $this->input->post('nrp', true),
+            "email" => $this->input->post('email', true),
+            "jurusan" => $this->input->post('jurusan')
+        ];
+
+        $this->db->insert('mahasiswa', $data);
+    }
+
+    public function hapusDataMahasiswa($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('mahasiswa');
+    }
+}
